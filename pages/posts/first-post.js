e72dc25249a7b6c74ab546da4d74ab05a9760ed9
Dev Micheal Khalifar <michealkhalifar@gmail.com>
2023-02-06 12:01:13 +0100

    modified:   pages/posts/first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,18 @@
-import Link from 'next/link';
+import Head from 'next/head';
+
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+                <script src="https://connect.facebook.net/en_US/sdk.js" />
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">← Back to home</Link>
+            </h2>
+        </>
+    );
 }
+  
